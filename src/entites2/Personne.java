70cad38fc62129063257adba97ffe5824560a65f
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-06 14:21:22 +0200

    Getters added + modifierPrenom and ModifierAdresse

--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -5,17 +5,40 @@ import entites.AdressePostale;
 public class Personne {
 	public String nom;
 	public String prenom;
-	public AdressePostale adressePostale;
+	public AdressePostale adresse;
 	
 	
 	public Personne(String nom, String prenom, AdressePostale adressePostale) {
         this.nom = nom;
         this.prenom = prenom;
-        this.adressePostale = adressePostale;
+        this.adresse = adressePostale;
     }
 	
 	public void AfficherPersonne() {
 		System.out.println(prenom + nom.toUpperCase());
 	}
 	
+	public void ModifierNom(String nouveau_nom) {
+		this.nom = nouveau_nom;
+	}
+	
+	public void ModifierPrenom(String nouveau_prenom) {
+		this.prenom = nouveau_prenom;
+	}
+	
+	public void ModifierAdresse (AdressePostale nouvelle_adresse) {
+		this.adresse = nouvelle_adresse;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public String getPrenom() {
+		return this.prenom;
+	}
+	
+	public AdressePostale getAdresse() {
+		return this.adresse;
+	}
 }
