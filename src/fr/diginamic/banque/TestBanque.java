8621a93ec30cb1fd4c4573b1484e0b48293ce3a5
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-09 12:11:57 +0200

    Compte and CompteTaux in TestBanque + toString() tested

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -8,7 +8,10 @@ public class TestBanque {
         Compte compte = new Compte("12345", 1000.0);
         CompteTaux compteTaux = new CompteTaux("67890", 2000.0, 0.03);
 
-       
+        Compte[] comptes = { compte, compteTaux };
+        for (Compte c : comptes) {
+            System.out.println(c);
+        }
 
     }
 }
