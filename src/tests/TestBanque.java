0f3f6c6f82d78530422ab303d9e7dfc5f1139215
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-09 10:28:41 +0200

    tests done + class Compte updated

---
+++ src/tests/TestBanque.java
@@ -0,0 +1,17 @@
+package tests;
+
+import fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+    public static void main(String[] args) {
+        // Créez une instance de la classe Compte avec un numéro et un solde de votre choix
+        Compte compte = new Compte("123456", 1000.0);
+
+        // Affichez l'instance créée avec System.out.println(...)
+        System.out.println("Numéro de compte : " + compte.getNumeroCompte());
+        System.out.println("Solde du compte : " + compte.getSolde());
+
+        // Que constatez-vous ?
+        // Vous devriez constater que les informations du compte (numéro et solde) sont affichées.
+    }
+}
