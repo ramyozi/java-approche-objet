f57bc4fb99f9a31fb94d2e9575b3fa4ded453046
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-11 11:48:18 +0200

    exo 1 done

--- src/listes/CreationListe.java
+++ src/listes/CreationListe.java
@@ -1,10 +1,16 @@
 package listes;
+import java.util.ArrayList; 
+
 
 public class CreationListe {
 
 	public static void main(String[] args) {
-
-
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		for(int i = 0; i<=100; i++) {
+			numbers.add(i);
+		}
+		System.out.println(numbers);
+		System.out.println("la taille de la liste: " + numbers.size());
 	}
 
 }
