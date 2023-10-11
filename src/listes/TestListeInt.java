872f2bea35b7f8ce2638a9987fe0cec083b95d48
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-11 12:14:06 +0200

    exo 3 done

--- src/listes/TestListeInt.java
+++ src/listes/TestListeInt.java
@@ -1,7 +1,7 @@
 package listes;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -9,20 +9,45 @@ public class TestListeInt {
 
 	public static void main(String[] args) {
 		
-		// diff ways to do it
-		int[] liste1 = {-1, 5, 7, 3, -2, 4, 8, 5};
-		List<Integer> liste2 = new ArrayList<Integer>();
-        List<Integer> liste3 = new ArrayList<>(Arrays.asList(-1, 5, 7, 3, -2, 4, 8, 5));
+		List<Integer> liste = new ArrayList<Integer>();
+		liste.add(-1);
+		liste.add(5);
+		liste.add(7);
+		liste.add(3);
+		liste.add(-2);
+        liste.add(4);
+        liste.add(8);
+        liste.add(5);
+        
+        
+        System.out.println("Contenu de la liste avant modification: " + liste);
+        System.out.println("taille de la liste : " + liste.size());
 
-        List<Integer> liste4 = new ArrayList<Integer>();
-        liste4.add(-1);
-        liste4.add(5);
-        liste4.add(7);
-        liste4.add(3);
-        liste4.add(-2);
-        liste4.add(4);
-        liste4.add(8);
-        liste4.add(5);
+        // une façon de faire
+        int max = 0;
+        for (int number : liste) {
+        	if (number>max) {
+        		max = number;
+        	}
+        }
+        System.out.println("le plus grand élément de la liste est " + max);
+
+        // une autre façon (en utilisant la biblio collections
+        int min = Collections.min(liste);
+        System.out.println("le plus petit élément de la liste est " + min);
+        liste.remove(Integer.valueOf(min));
+        System.out.println("Liste après suppression du plus petit élément : " + liste);
+
+        
+        for (int i = 0; i < liste.size(); i++) {
+            int element = liste.get(i);
+            if (element < 0) {
+            	liste.set(i, Math.abs(element));
+            }
+        }
+        
+        System.out.println("Contenu de la liste apres modification: " + liste);
+
 	}
 
 }
